e3c619899383d1227fcad07c671501193b6a3ad2
eduardo <eduardovinoles@gmail.com>
2022-03-30 22:37:20 -0300

    wip

--- src/routes/technical-assessment/part-a.js
+++ src/routes/technical-assessment/part-a.js
@@ -3,9 +3,10 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 const fs = require('fs');
-const getFiles = require('../../utils/files')
-router.get('/', async (req, res) => {
+const getFiles = require('../../utils/files');
+const { toNamespacedPath } = require('path');
 
+router.get('/', async (req, res) => {
 
     try {
         //requiring path and fs modules
@@ -17,29 +18,37 @@ router.get('/', async (req, res) => {
         let response = {
             'auth_module':
             {
-                'authn.provider_1': ['./u1.json', './u2.json'],
-                'authn.provider_2': ['./u3.json', './u4.json', './u5.json']
+
             },
             'content_module':
             {
-                'authz.provider_1': ['./u1.json', './u3.json'],
-                'authz.provider_2': ['./u2.json', './u4.json'],
-                'authz.provider_3': ['./u5.json']
+
             }
         }
 
         listOfUsers.forEach((file) => {
 
-            if (response.auth_module[file.provider.auth_module]){
-                // Ya existe, lo agregamos
-                }else{
-                // No existe, lo creamos y agregamos
-                }
-                
-            console.log(file)
+            if (!response.auth_module[file.provider.auth_module]) {
+                response.auth_module[file.provider.auth_module] = []
+            }
+
+            response.auth_module[file.provider.auth_module].push(file.originFile)
+
+            if (!response.content_module[file.provider.content_module]) {
+                response.content_module[file.provider.content_module] = []
+            }
+
+            response.content_module[file.provider.content_module].push(file.originFile)
+
         });
 
-        res.send(listOfUsers)
+        
+        console.log(response.length)
+
+
+
+
+        res.send(response)
     } catch (error) {
         res.send(error)
     }
--- src/utils/files.js
+++ src/utils/files.js
@@ -3,24 +3,21 @@ const fs = require('fs');
 
 const getFiles = async (directoryPath) => {
 
-
         //passsing directoryPath and callback function
         files = fs.readdirSync(directoryPath);
-
+        
         let listOfUsers = []
-
+        
         files.forEach((file) => {
 
             let rawdata = fs.readFileSync(path.join(directoryPath,file));
             let userProviders = JSON.parse(rawdata);
-            listOfUsers.push(userProviders)
-          
+            userProviders.originFile = "./" + file
+            listOfUsers.push(userProviders)          
         });
       
         return listOfUsers
-   
-       
-   
+      
 }
 
 module.exports = getFiles;
